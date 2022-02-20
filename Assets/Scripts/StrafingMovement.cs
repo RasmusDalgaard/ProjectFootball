c7156c671b95b5cc821fbb206c67d1b1a305b893
Janussr <Janusstivang@gmail.com>
2022-02-20 13:47:43 +0100

    goal score prefab

--- Assets/Scripts/StrafingMovement.cs
+++ Assets/Scripts/StrafingMovement.cs
@@ -11,19 +11,12 @@ public class StrafingMovement : MonoBehaviour
     private int current;
 
 
-    void Start()
-    {
 
-
-    }
-
-    void Update()
-    {
-
+    void Update()   {
 
        if (transform.position != target[current].position)
         {
-            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed);
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
             GetComponent<Rigidbody>().MovePosition(pos);
         }
             else current = (current + 1) % target.Length;
